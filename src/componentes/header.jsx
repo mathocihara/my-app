c89d6cf89472ca33944b2ab64c6bc3bc184854c3
mathocihara <matheusyukio.hocihara@gmail.com>
2022-07-27 01:23:26 -0300

    estilização do header

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -1,37 +1,40 @@
 import React from 'react';
+
+import './header.css'
 function Header() {
-    return ( 
-        
-        
-        <header>
-            <div>
-                <p>uma seleção de produtos</p>
-                <h1>especial para você</h1>
-                <p>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
-            </div>
-            <div>
-                <button>
+return (
+
+
+<header>
+    <div className='header'>
+        <div className='text'>
+            <p>uma seleção de produtos</p>
+            <h1>especial para você</h1>
+            <p>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+        </div>
+        <div className='buttonHeader'>
+            <button>
                 Conheça a Linx
-                </button>
-                
-                <button>
+            </button>
+
+            <button>
                 Ajude o algorítimo
-                </button>                
-                
-                <button>
+            </button>
+
+            <button>
                 Seus produtos
-                </button>                
-                
-                <button>
+            </button>
+
+            <button>
                 Compartilhe
-                </button>
-            </div>
+            </button>
+        </div>
+    </div>
+</header>
 
-    </header>
 
 
-    
-     );
+);
 }
- 
+
 export default Header;
