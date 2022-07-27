aff437a24bc8a3c001f8c52a098c574f76f8d894
mathocihara <matheusyukio.hocihara@gmail.com>
2022-07-27 18:37:43 -0300

    estilização do grid

--- src/componentes/grid.jsx
+++ src/componentes/grid.jsx
@@ -1,41 +1,74 @@
 import React from 'react';
 import { Component } from 'react';
 import Api from './api.jsx';
+import './grid.css'
 
 class Grid extends Component {
 
-    state={
-        produtos:[],
-      }
-    
-      async componentDidMount(){
-        const response = await Api.get('');
-    console.log(response.data.products)
-        this.setState({ produtos: response.data.products})
-      }
-    
-      render(){
+state={
+produtos:[],
+}
 
-        const {produtos} = this.state;
-      return (
-        <div className="App">
+async componentDidMount(){
+const response = await Api.get('');
+console.log(response.data.products)
+this.setState({ produtos: response.data.products})
+}
 
-             <h1> Listar Produtos</h1>
-             {produtos.map(produto =>{
-                <div>
-                 key={produto.id}
-             <strong>Nome Produto</strong>
-             <p>{produto.name}</p>
-            {/* {produto.description} */}
-                </div>
+render(){
 
+const {produtos} = this.state;
+return (
+<div className="AppGrid">
 
-                
-           })}
+<h3 className='subTituloGrid'>Sua seleção especial</h3>  
+  
 
-        </div>
-      );
-    }
+  <ul>
+  {produtos.map(produto =>{
+  return(
+  <div className="card" key={produto.id}  >
+    <strong>Nome Produto</strong>
+    <p>{produto.name}</p>
+    <strong>Descrição do Produto</strong>
+    <p>{produto.description}</p>
+    <strong>Valor Antigo</strong>
+    <p>{produto.oldPrice}</p>
+    <strong>Valor</strong>
+    <p>{produto.price}</p>
+    <strong>Quantidade de Parcelas</strong>
+    <p>{produto.installments.count}</p>
+    <strong> Parcelas</strong>
+    <p>{produto.installments.value}</p>
+    <button className='buttonGrid'>Comprar</button>
+    </div>
+  
+  )
+  })}</ul>
+    <div className='informaçõesGerais'>
+    <button className='buttonGridProdutos'>Ainda mais produtos aqui!</button>
+    <h3>Compartilhe a novidade</h3>
+    <p>Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</p>
+    <form>
+    <div className='formularioPerguntas'>
+    <label  className='labeFormGrid'>Nome do seu amigo:
+      <input type="text" />
+    </label>
+
+    <label  className='buttonFormGrid'>Email:
+      <input type="text" />
+    </label>
+    </div>
+    <div className='buttonFormFinal'>
+    <button className='buttonFormEnvio'>Enviar agora</button>
+    </div>
+    </form>
+
+    </div>
+
+</div>
+);
+}
 }
 
 export default Grid;
