ca1cbe5479ab8205f6fac5403d8d73b0b2e0883a
mathocihara <matheusyukio.hocihara@gmail.com>
2022-07-26 23:07:12 -0300

    criação do projeto e preparo do react

---
+++ src/componentes/header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+function Header() {
+    return ( 
+        
+        
+        <header>
+            <div>
+                <p>uma seleção de produtos</p>
+                <h1>especial para você</h1>
+                <p>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+            </div>
+            <div>
+                <button>
+                Conheça a Linx
+                </button>
+                
+                <button>
+                Ajude o algorítimo
+                </button>                
+                
+                <button>
+                Seus produtos
+                </button>                
+                
+                <button>
+                Compartilhe
+                </button>
+            </div>
+
+    </header>
+
+
+    
+     );
+}
+ 
+export default Header;
