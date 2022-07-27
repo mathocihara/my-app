7425bc2fbad0668c25571d229c00026e8e2def1d
mathocihara <matheusyukio.hocihara@gmail.com>
2022-07-27 15:26:13 -0300

    adicao de fonte e restrutura dos para API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import Header from './componentes/header.jsx';
 import Main from './componentes/main.jsx';
-import Footer from './componentes/footer.jsx'
-function App() {
+import Footer from './componentes/footer.jsx';
+import Grid from './componentes/grid.jsx';
+
+function App (){
+
+//   render(){
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <Grid/>
       <Footer/>
     </div>
   );
 }
+
 
 export default App;
